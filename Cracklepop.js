235b14254fd51b79ae9dcc64239722cd77f0725c
Nina Baculinao <nb2406@columbia.edu>
2014-05-06 01:52:28 +0800

    Add js version of crackle pop for comparison

---
+++ Cracklepop.js
@@ -0,0 +1,11 @@
+for (var counter = 0; counter < 100; counter++) {
+  if (counter % 15 == 0)
+    print("Cracklepop");
+  else if (counter % 3 == 0)
+    print("Crackle");
+  else if (counter % 5 == 0)
+    print ("Pop")
+  else
+    print (counter);
+    
+}
