b5868319b9ed6264697fe1e3c17ea890c54cd770
Nina Baculinao <nb2406@columbia.edu>
2014-05-13 03:23:35 +0800

    Substantiate function more completely

--- 46-Simple-Ex/8-palindrome.py
+++ 46-Simple-Ex/8-palindrome.py
@@ -2,18 +2,20 @@ def reverse(text):
     return text[::-1]
 
 def is_palindrome(text):
-	''' (str) -> bool
+	''' (str) -> bool -> str
 	Defines a function is_palindrome() that recognizes palindromes
 	(i.e. words that look the same written backwards)
 	'''
 	text = text.lower()
-	punctuation = ['"', "'", '.', ',', '?', '!', ':', ';', '-', ')', '(', ']', '[', '\\', '/']
+	punctuation = ['"', "'", '.', ',', ' ', '?', '!', ':', ';', '-', ')',
+				   '(', ']', '[', '\\', '/']
 	for x in punctuation: # for loop checks text for x items from tuple
 		text = text.replace(x, '') # removes x items
-	return text == reverse(text) 
+	if text == reverse(text):
+		return "Yes, it is a palindrome" # if true
+	else:
+		return "No, it is not a palindrome"
 
 something = raw_input("Enter text: ")
-if is_palindrome(something): # if true
-    print "Yes, it is a palindrome"
-else:
-    print "No, it is not a palindrome"
+print is_palindrome(something)
+
