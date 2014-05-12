9659c5f0d592ce6ba20abc0fa7ff6bd4414ce779
Nina Baculinao <nb2406@columbia.edu>
2014-05-12 10:26:51 +0800

    Add ex5 translate function

---
+++ 46-Simple-Ex/5-translate.py
@@ -0,0 +1,19 @@
+# encoding=utf-8
+
+# Defines function translate() that will translate a text into "rövarspråket"
+# (Swedish for "robber's language"), i.e. double every consonant and place
+# an occurrence of "o" in between.
+
+def translate(text, consonants = 'bcdfghjklmnpqrstvwxyz'):
+	for x in consonants:
+		text = text.replace(x, x + 'o' + x)
+	return text
+
+print u"""This will translate a text into 'rövarspråket' (Swedish
+for robber's  language) and double every consonant with
+'o' in between. For example, 'This is fun' becomes : """
+print translate('This is fun')
+print ""
+
+text = str(raw_input('Enter some text : '))
+print 'Translation : ' + str(translate(text))
