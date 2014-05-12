b08d9228f640cbe1cb0ff4551baffd220584de3b
Nina Baculinao <nb2406@columbia.edu>
2014-05-12 21:59:56 +0800

    Add ex 3 length function for list or string

---
+++ 46-Simple-Ex/3-length.py
@@ -0,0 +1,13 @@
+def length(given):
+	''' (list or str) -> int
+	Defines a function that computes the length of a given list or string. 
+	Assuming built-in len() function doesn't exist.
+	'''
+
+	counter = 0
+	for item in given:
+		counter += 1
+	return counter
+
+something = input("Enter string or list : ")
+print ("Length is " + str(length(somelist)))
