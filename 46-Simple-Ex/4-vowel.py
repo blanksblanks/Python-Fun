366cb6a3c92176af85501924ff585ff28b261b3e
Nina Baculinao <nb2406@columbia.edu>
2014-05-12 10:01:55 +0800

    Add description to function and user-defined error

--- 46-Simple-Ex/4-vowel.py
+++ 46-Simple-Ex/4-vowel.py
@@ -1,3 +1,14 @@
+# Defines a function that takes a character (i.e. a string of length 1)
+# and returns TRue if it is a vowel, False if otherwise
+
+# HOW TO DEFINE EXCEPTION CLASS: class ErrorName(Exception):
+# def __init__(self, vars): Exception.__init__(self) self.var = var
+
+# HOW TO RAISE ERROR: try: raise...except ErrorName as err:... else:
+
+def is_vowel(letter):
+	return letter == ('a' or 'e' or 'i' or 'o' or 'u')
+
 class LongInputException(Exception):
 	'''A user-defined exception class.'''
 	def __init__(self, length, atleast):
@@ -5,12 +16,9 @@ class LongInputException(Exception):
 		self.length = length
 		self.reqlength = reqlength
 
-def is_vowel(letter):
-	return letter == ('a' or 'e' or 'i' or 'o' or 'u')
-
 try:
 	text = str(raw_input('Enter a character : '))
-	if len(text) != 1:
+	if len(text) != 1: # if string isn't length 1
 		raise LongInputException(len(text), 1)
 except LongInputException as err:
 		print ('LongInputException: The input was ' + \
