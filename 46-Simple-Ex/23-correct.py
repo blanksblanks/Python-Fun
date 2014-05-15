1fd826ad0f5eb08df70c1720bcaa5e14310c9300
Nina Baculinao <nb2406@columbia.edu>
2014-05-15 21:27:31 +0800

    Add note to self

--- 46-Simple-Ex/23-correct.py
+++ 46-Simple-Ex/23-correct.py
@@ -6,6 +6,7 @@ def correct(text):
 	to it that 1) two or more occurrences of the space character is compressed into one,
 	and 2) inserts an extra space after a period if it is directly followed by a letter.
 	Uses regular expression: re.sub(pattern, repl, string, count=0, flags=0)
+	Note: read more about RE at https://docs.python.org/2/library/re.html
 	'''
 	text = re.sub(r'( )+', r'\1', text) # reduces 2+ spaces to 1
 	text = re.sub(r'[.]([a-zA-Z])', r'. \1', text) # inserts space after '.Aa-Zz'
