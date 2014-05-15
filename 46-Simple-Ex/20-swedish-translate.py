e2b803adca3404034525a009e8cf0867f889911c
Nina Baculinao <nb2406@columbia.edu>
2014-05-15 12:25:01 +0800

    add ex 20 swedish translate function

---
+++ 46-Simple-Ex/20-swedish-translate.py
@@ -0,0 +1,25 @@
+# encoding=utf-8
+
+def swedish_translate(text):
+	''' (str) -> str
+	Defines a function swedish_translate() that takes a list of English words and
+	returns a Swedish words. The dictionary and its key:value pairs represent a small
+	bilingual lexicon of English and Swedish words used in a holiday card.
+	'''
+	dictionary = {"merry":"god",
+                  "christmas":"jul",
+                  "and":"och",
+                  "happy":"gott",
+                  "new":"nytt",
+                  "year":"år"}
+	text = text.lower()
+	for word in text.split(" "):
+		if (str(dictionary.get(word))) != False: #get(word) same as [word]
+			text = text.replace(word, str(dictionary[word]))
+		else:
+			text = "This could not be translated."
+	return text
+
+something = "Merry Christmas and Happy New Year"
+print "English : " + something
+print "Swedish : " + swedish_translate(something)
