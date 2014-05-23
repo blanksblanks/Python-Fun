572469eedb1896e6473d9042319426eeba688d96
Nina Baculinao <nb2406@columbia.edu>
2014-05-23 16:54:25 -0400

    Add if name = main to ex8

--- 46-Simple-Ex/ex8.py
+++ 46-Simple-Ex/ex8.py
@@ -1,3 +1,6 @@
+def reverse(text):
+    return text[::-1]
+
 def is_palindrome(text):
 	''' (str) -> bool -> str
 	Defines a function is_palindrome() that recognizes palindromes
@@ -9,10 +12,21 @@ def is_palindrome(text):
 	for x in punctuation: # for loop checks text for x items from tuple
 		text = text.replace(x, '') # removes x items
 	if text == reverse(text):
-		return "Yes, it is a palindrome" # if true
+		return True
 	else:
-		return "No, it is not a palindrome"
+		return False
 
-something = raw_input("Enter text: ")
-print is_palindrome(something)
+if __name__ == "__main__":
+	'''
+	If this file is being imported from another module, __name__ will be
+	set to the module's name." By doing the main check, you can have that
+	code only execute when you want to run the module as a program and not
+	have it execute when you just want to import your module and call your
+	functions themselves.
+	'''
+	something = raw_input("Enter text: ")
+	if is_palindrome(something) == True:
+		print "Yes, it is a palindrome"
+	else:
+		print "No, it is not a palindrome"
 
